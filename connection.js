dca29043a7406f41e85aad987bbdd693c7c0814e
Saly <swalynerii@gmail.com>
2023-12-23 20:11:07 -0600

    fix: database connection fixed

--- connection.js
+++ connection.js
@@ -2,16 +2,7 @@ require("dotenv").config();
 const { MongoClient } = require("mongodb");
 const mongoose = require("mongoose");
 
-async function main(callback) {
-  const URI = process.env.MONGO_URI;
-  const dbconnect = mongoose.connect(URI);
+const URI = process.env.MONGO_URI;
+const dbconnect = mongoose.connect(URI);
 
-  try {
-    await callback(dbconnect);
-  } catch (e) {
-    console.error(e);
-    throw new Error("Unable to Connect to Database");
-  }
-}
-
-module.exports = main;
+module.exports = dbconnect;
--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,55 @@
-'use strict';
+"use strict";
+
+const IssueModel = require("../models").Issue;
+const ProjectModel = require("../models").Project;
 
 module.exports = function (app) {
+  app
+    .route("/api/issues/:project")
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
+    .get(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .post(function (req, res){
-      let project = req.params.project;
-      
+
+    .post(async (req, res) => {
+      let projectName = req.params.project;
+      const { issue_title, issue_text, created_by, assigned_to, status_text } =
+        req.body;
+
+      if (!issue_title || !issue_text || !created_by) {
+        res.json({ error: "required field(s) missing" });
+        return;
+      }
+      try {
+        let projectModel = await ProjectModel.findOne({ name: projectName });
+        if (!projectModel) {
+          projectModel = new ProjectModel({ name: projectName });
+          projectModel = await projectModel.save();
+        }
+
+        const issueModel = new IssueModel({
+          projectId: projectModelrojectModel._id,
+          issue_title: issue_title || "",
+          issue_text: issue_text || "",
+          created_on: new Date(),
+          updated_on: new Date(),
+          created_by: created_by || "",
+          assigned_to: assigned_to || "",
+          open: true,
+          status_text: status_text || "",
+        });
+        const issue = await issueModel.save();
+        res.json(issue);
+      } catch (err) {
+        res.json({ error: "enter valid data" });
+      }
     })
-    
-    .put(function (req, res){
+
+    .put(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .delete(function (req, res){
+
+    .delete(function (req, res) {
       let project = req.params.project;
-      
     });
-    
 };
